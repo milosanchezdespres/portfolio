b4fa949d5644b8424c02c2f82b1c4b4bff89d04d
milosanchezdespres <milosanchezdespres@gmail.com>
2025-06-23 20:41:47 +0200

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import Block from '@/app/components/Block'
 export default function Page() {
   return (
     <BlockContainer>
-      <Block color="#34d399" hover="#ffde00" text ="block title" src="/vercel.svg" />
-      <Block color="#f87171" hover="#60a5fa" text ="block title" src="/vercel.svg" />
-      <Block color="#60a5fa" hover="#f87171" text ="block title" src="/vercel.svg" />
-      <Block color="#ffde00" hover="#f87171" text ="block title" src="/vercel.svg" />
-      <Block color="#ffde00" hover="#f87171" text ="block title" src="/vercel.svg" />
-      <Block color="#60a5fa" hover="#f87171" text ="block title" src="/vercel.svg" />
-      <Block color="#34d399" hover="#ffde00" text ="block title" src="/vercel.svg" />
-      <Block color="#f87171" hover="#60a5fa" text ="block title" src="/vercel.svg" />
+      <Block color="#34d399" hover="#ffde00" text ="プロジェクト名" src="/vercel.svg" />
+      <Block color="#f87171" hover="#60a5fa" text ="プロジェクト名" src="/vercel.svg" />
+      <Block color="#60a5fa" hover="#f87171" text ="プロジェクト名" src="/vercel.svg" />
+      <Block color="#ffde00" hover="#f87171" text ="プロジェクト名" src="/vercel.svg" />
+      <Block color="#ffde00" hover="#f87171" text ="プロジェクト名" src="/vercel.svg" />
+      <Block color="#60a5fa" hover="#f87171" text ="プロジェクト名" src="/vercel.svg" />
+      <Block color="#34d399" hover="#ffde00" text ="プロジェクト名" src="/vercel.svg" />
+      <Block color="#f87171" hover="#60a5fa" text ="プロジェクト名" src="/vercel.svg" />
     </BlockContainer>
   )
 }
